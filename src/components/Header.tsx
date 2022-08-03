9816183d4d0aa42a8b9cb874b24c91822841e584
Dmitri <dmitribeluy@gmail.com>
2022-08-03 23:17:57 +0300

    verstka na gridah

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className='header'>
+            <img src='https://www.logodesign.net/mascot_logo/cool-mascot-of-koala-face-6148ld.png' alt=""/>
+        </header>
+    )
+
+}
+export default Header;
