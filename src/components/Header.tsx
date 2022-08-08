7751e8277acab6f8bb961cb044bae218c1edece6
Dmitri <dmitribeluy@gmail.com>
2022-08-08 21:53:12 +0300

    14 urok_css_modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 function Header() {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src='https://www.logodesign.net/mascot_logo/cool-mascot-of-koala-face-6148ld.png' alt=""/>
         </header>
     )
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 function Profile() {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div><img
                 src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"
                 alt=""/>
@@ -15,11 +16,11 @@ function Profile() {
                 <div>
                     New post
                 </div>
-                <div>
-                    <div>
+                <div className={s.posts}>
+                    <div className={s.item}>
                         post 1
                     </div>
-                    <div>
+                    <div className={s.item}>
                         post 2
                     </div>
                 </div>
@@ -28,4 +29,5 @@ function Profile() {
     )
 
 }
+
 export default Profile;
